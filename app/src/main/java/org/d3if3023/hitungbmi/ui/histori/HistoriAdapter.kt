004f0865b1ff64553ff8f529ee6f91b4a226979b
citrapangestu <citrapangesu.cp@gmail.com>
2021-04-25 04:38:28 +0700

    Menghapus data dari database

--- app/src/main/java/org/d3if3023/hitungbmi/ui/histori/HistoriAdapter.kt
+++ app/src/main/java/org/d3if3023/hitungbmi/ui/histori/HistoriAdapter.kt
@@ -11,17 +11,16 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class HistoriAdapter : RecyclerView.Adapter<HistoriAdapter.ViewHolder>() {
+
     private val data = mutableListOf<BmiEntity>()
+
     fun updateData(newData: List<BmiEntity>) {
         data.clear()
         data.addAll(newData)
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemHistoriBinding.inflate(inflater, parent, false)
         return ViewHolder(binding)
@@ -36,28 +35,20 @@ class HistoriAdapter : RecyclerView.Adapter<HistoriAdapter.ViewHolder>() {
     }
 
     class ViewHolder(
-        private val binding: ItemHistoriBinding) : RecyclerView.ViewHolder(binding.root) {
-        private val dateFormatter = SimpleDateFormat(
-            "dd MMMM yyyy",
-            Locale("id", "ID")
-        )
+        private val binding: ItemHistoriBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private val dateFormatter = SimpleDateFormat("dd MMMM yyyy", Locale("id", "ID"))
 
         fun bind(item: BmiEntity) = with(binding) {
             tanggalTextView.text = dateFormatter.format(Date(item.tanggal))
+
             val hasilBmi = HitungBmi.hitung(item)
             kategoriTextView.text = hasilBmi.kategori.toString()
-            bmiTextView.text = root.context.getString(
-                R.string.bmi_x,
-                hasilBmi.bmi
-            )
-            beratTextView.text = root.context.getString(
-                R.string.berat_x,
-                item.berat
-            )
-            tinggiTextView.text = root.context.getString(
-                R.string.tinggi_x,
-                item.tinggi
-            )
+            bmiTextView.text = root.context.getString(R.string.bmi_x, hasilBmi.bmi)
+
+            beratTextView.text = root.context.getString(R.string.berat_x, item.berat)
+            tinggiTextView.text = root.context.getString(R.string.tinggi_x, item.tinggi)
             val stringRes = if (item.isMale) R.string.pria else R.string.wanita
             genderTextView.text = root.context.getString(stringRes)
         }
